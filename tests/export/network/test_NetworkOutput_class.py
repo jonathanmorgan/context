7676b4325df0c13bc58fbf91b380cba38aeb88dd
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2020-01-09 05:04:46 +0000

    moved shared test case validation method into python_utilities parent.

--- tests/export/network/test_NetworkOutput_class.py
+++ tests/export/network/test_NetworkOutput_class.py
@@ -35,10 +35,11 @@ from context.tests.export.network.test_helper import TestHelper
 
 # python_utilities
 from python_utilities.booleans.boolean_helper import BooleanHelper
+from python_utilities.django_utils.django_test_case_helper import DjangoTestCaseHelper
 from python_utilities.exceptions.exception_helper import ExceptionHelper
 
 
-class NetworkOutputTest( django.test.TestCase ):
+class NetworkOutputTest( DjangoTestCaseHelper ):
     
 
     #----------------------------------------------------------------------------
@@ -48,6 +49,7 @@ class NetworkOutputTest( django.test.TestCase ):
 
     # DEBUG
     DEBUG = False
+    LOGGER_NAME = "context.tests.export.network.test_NetworkOutput_class.py.NetworkOutputTest"
 
     # CLASS NAME
     CLASS_NAME = "NetworkOutputTest"
@@ -376,77 +378,10 @@ class NetworkOutputTest( django.test.TestCase ):
         network_data = test_instance.render_network_data()
         network_data_char_count = len( network_data )
         
-        # load the data file that contains the goal output.
+        # validate against test file.
         reference_data_file_path = TestHelper.FILE_PATH_NETWORK_DATA_REQUEST_BASIC_TSV_OUTPUT
-        try:
+        self.validate_string_against_file_contents( network_data, reference_data_file_path )
         
-            # yes.  Output to file.
-            with open( reference_data_file_path ) as reference_data_file:
-            
-                # read contents of file
-                reference_data = reference_data_file.read()
-                
-            #-- END with open( reference_data_file_path, "w" ) as reference_data_file --#
-            
-        except:
-        
-            # Could not read reference data.
-            status_message = "ERROR - Exception thrown reading network data from {}.".format( reference_data_file_path )
-            exception_instance = sys.exc_info()[0]
-            self.log_exception( exception_instance, message_IN = status_message, method_IN = me, logger_name_IN = self.LOGGER_NAME, do_print_IN = debug_flag, log_level_code_IN = logging.ERROR )
-            
-        #-- END try...except around writing output to file --#
-
-        # lengths and content should be the same.        
-        reference_data_char_count = len( reference_data )
-        
-        # diff
-        temp_data = network_data.strip()
-        network_data_lines = temp_data.splitlines()
-        temp_data = reference_data.strip()
-        reference_data_lines = temp_data.splitlines()
-        diff_count = 0
-        for diff_output_line in difflib.unified_diff( network_data_lines,
-                                                      reference_data_lines,
-                                                      fromfile = 'rendered_data',
-                                                      tofile = 'reference_data',
-                                                      lineterm = '' ):
-        
-            # print the line
-            print( diff_output_line )
-            diff_count += 1
-            
-        #-- END loop over diff output. --#
-        
-        # diff_count should be 0
-        test_value = diff_count
-        should_be = 0
-        error_string = "diff count was {}, should be {}.".format( test_value, should_be )
-        self.assertEqual( test_value, should_be, msg = error_string )
-        
-        # line counts?
-        network_data_line_count = len( network_data_lines )
-        reference_data_line_count = len( reference_data_lines )
-        test_value = network_data_line_count
-        should_be = reference_data_line_count
-        error_string = "line counts are not equal. rendered: {}; reference: {}.".format( test_value, should_be )
-        self.assertEqual( test_value, should_be, msg = error_string )
-
-        # loop and compare line-by-line
-        for line_index in range( network_data_line_count ):
-        
-            # get lines
-            network_data_line = network_data_lines[ line_index ]
-            reference_data_line = reference_data_lines[ line_index ]
-                
-            # lines should be the same
-            test_value = network_data_line
-            should_be = reference_data_line
-            error_string = "data line not equal:\n- rendered: {}\n- reference: {}.".format( test_value, should_be )
-            self.assertEqual( test_value, should_be, msg = error_string )
-            
-        #-- END loop over lines in file --#
-
     #-- END test method test_render_network_data() --#
         
         
